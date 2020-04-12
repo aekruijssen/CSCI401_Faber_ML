cdcfebb416fef717f44885383e2c31790d534fb4
Jingyun Yang <jingyuny@usc.edu>
2020-04-12 03:49:14 -0700

    Removed unnecessary files.

--- methods/learning/datasets/__init__.py
+++
@@ -1,6 +0,0 @@
-def get_data_loader_by_name(name):
-    if name == 'maps':
-        from .maps.maps_data_loader import MapsDataLoader
-        return MapsDataLoader
-    else:
-        raise ValueError("Data loader named '{}' not found!".format(name))
